d441376cb770a637ff04cde4d4e5d18288a6ca1d
beatorizu <betrizaf@gmail.com>
2016-06-13 09:14:50 -0300

    Updated bash script [the_right_way].
    Removing ${BASH_SOURCE[*]}.

--- the_right_way.sh
+++ the_right_way.sh
@@ -3,5 +3,5 @@
 echo "$0"
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$SCRIPT_DIR"
-"${BASH_SOURCE[*]}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "$SCRIPT_DIR"
