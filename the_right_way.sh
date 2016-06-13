16a8a1f4b0c88d6cba6cdde11fbfaad4d72a05b0
beatorizu <betrizaf@gmail.com>
2016-06-13 15:07:20 -0300

    Updated bash script [the_right_way].
    Testing getopts to get flag.

--- the_right_way.sh
+++ the_right_way.sh
@@ -5,3 +5,18 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$SCRIPT_DIR"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$SCRIPT_DIR"
+
+while getopts ":r" opt; do
+    case $opt in
+        r)
+            test=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+if [ ${test} ]; then
+    echo "-a was triggered!" >&2
+fi
